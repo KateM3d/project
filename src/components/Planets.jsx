fd287a5ed089751d7fcd46f4be6fa47c3a078d42
KateM3d <ekaterina8medvedeva@gmail.com>
2022-02-17 09:32:59 -0500

    added state

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,5 +1,18 @@
-import data from "../data";
+import { useState } from "react";
+import { data } from "../data";
+import Cards from "./Cards";
 
 export default function Planets() {
-  return <h2>Planets</h2>;
+  const [planets, setPlanets] = useState(data);
+  return (
+    <>
+      <h2>Planets</h2>
+      <div className="container">
+        {planets.map((item) => {
+          const { id, img, planet, info } = item;
+          return <Cards key={id} image={img} planet={planet} info={info} />;
+        })}
+      </div>
+    </>
+  );
 }

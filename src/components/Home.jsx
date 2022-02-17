886554951a090c25ec0df610034e30ca2337735b
KateM3d <ekaterina8medvedeva@gmail.com>
2022-02-17 10:23:00 -0500

    working on home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <h2>Home</h2>;
+  return (
+    <>
+      <h2>Home</h2>
+      <div className="home">
+        <h2 className="home-header">
+          There are many planetary systems like ours in the universe, with
+          planets orbiting a host star. Our planetary system is named the "solar
+          system" because our Sun is named Sol, after the Latin word for Sun,
+          "solis," and anything related to the Sun we call "solar."
+        </h2>
+        <p className="home-text">
+          Our solar system consists of our star, the Sun, and everything bound
+          to it by gravity – the planets Mercury, Venus, Earth, Mars, Jupiter,
+          Saturn, Uranus, and Neptune; dwarf planets such as Pluto; dozens of
+          moons; and millions of asteroids, comets, and meteoroids. Beyond our
+          own solar system, we have discovered thousands of planetary systems
+          orbiting other stars in the Milky Way.
+        </p>
+      </div>
+    </>
+  );
 }

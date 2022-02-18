a51cb0f2c95bbeecc65c5f5e215715d653d96b5c
KateM3d <ekaterina8medvedeva@gmail.com>
2022-02-18 12:17:18 -0500

    working on search

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -6,26 +6,9 @@ export default function Planets() {
   const [planets, setPlanets] = useState(data);
   const [search, setSearch] = useState();
 
-  const handleSearchChange = (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
-    let searchValue = e.target.value;
-    setSearch(searchValue);
-    console.log(searchValue);
-    console.log(search);
-
-    const filterPlanets = planets.filter(
-      (el) => el.planet.toLowerCase() === searchValue
-    );
-
-    console.log(filterPlanets);
-
-    return setPlanets(filterPlanets);
-  };
-
   return (
     <>
-      <div className="search">
+      {/* <div className="search">
         <input
           value={search}
           className="search-input"
@@ -33,7 +16,7 @@ export default function Planets() {
           placeholder="what are you looking for?..."
           onChange={(e) => handleSearchChange(e)}
         />
-      </div>
+      </div> */}
       <div className="container">
         {planets.map((item) => {
           const { id, img, planet, info, link } = item;
